731269dcfe9d31473ccb3b5b3d92d8e9ad44717f
Anastasiia Kurdydyk <37546139+akurdydy@users.noreply.github.com>
2019-11-24 19:16:19 +0100

    Create survey.js

---
+++ survey.js
@@ -0,0 +1,82 @@
+Survey
+    .StylesManager
+    .applyTheme("default");
+
+var json = {
+    questions: [
+        {
+            type: "matrix",
+            name: "Quality",
+            title: "Please indicate if you agree or disagree with the following statements",
+            columns: [
+                {
+                    value: 1,
+                    text: "Strongly Disagree"
+                }, {
+                    value: 2,
+                    text: ""
+                }, {
+                    value: 3,
+                    text: ""
+                }, {
+                    value: 4,
+                    text: ""
+                }, {
+                    value: 5,
+                    text: ""
+                }, {
+                    value: 6,
+                    text: ""
+                }, {
+                    value: 7,
+                    text: "Strongly Agree"
+                }
+            ],
+            rows: [
+                {
+                    value: "intrinsic1",
+                    text: "I avoid behaviors that would hurt the environment due to my personally important beliefs"
+                }, {
+                    value: "intrinsic2",
+                    text: "I try to behave in proenvironmental ways because it is personally important to me"
+                }, {
+                    value: "intrinsic3",
+                    text: "I am personally motivated by my concern for the environment to engage in proenvironmental behavior"
+                }, {
+                    value: "intrinsic4",
+                    text: "I try to make decisions that are good for the environment because concern for the environment is central to my core value"
+                },
+                
+                {
+                    value: "external1",
+                    text: "If I engage in behaviors that may be harmful to the environment, I feel like others judge me negatively"
+                }, {
+                    value: "external2",
+                    text: "I am motivated to engage in behaviors that help protect the environment so that other people think I am a good person"
+                }, {
+                    value: "external3",
+                    text: "If I didn’t engage in proenvironmental behaviors, I would worry that others would think of me as a bad person"
+                }, {
+                    value: "external4",
+                    text: "It is important to me that other people are aware of the efforts I make to help protect the environment"
+                }, {
+                    value: "external5",
+                    text: "I try to make proenvironmental decisions to avoid social disapprovt is important to me that other people are aware of the efforts"
+                }
+                
+            ]
+        }
+    ]
+};
+
+window.survey = new Survey.Model(json);
+
+survey
+    .onComplete
+    .add(function (result) {
+        document
+            .querySelector('#surveyResult')
+            .textContent = "Result JSON:\n" + JSON.stringify(result.data, null, 3);
+    });
+
+$("#surveyElement").Survey({model: survey});
