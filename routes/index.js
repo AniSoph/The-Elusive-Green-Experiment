5fcdce928bfb71b69d656df56e4be2a5e5bfbe21
Anastasiia Kurdydyk <37546139+akurdydy@users.noreply.github.com>
2019-12-13 18:01:10 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -6,48 +6,59 @@ var express = require('express');
 
 var router = express.Router();
 
-/* GET home page. */
+/*Instructions - GET home page.*/
 router.get('/', function(req, res, next ) {
   res.render('index', { title: 'Express' });
 });
 
- /*Product Category Selection page - GET REQUEST*/
- router.get('/selectcategory',function(req,res, next ) {
-    res.render('productCategory');
- });
+/*Preferences Survey - GET REQUEST.*/
+router.get('/survey', function(req, res, next ) {
+  res.render('preferenceSurvey');
+});
 
- /*Product Choice Selection page - GET REQUEST*/
- router.get('/selectproduct',function(req,res, next ) {
-   res.render('productChoice');
-  });
+/*Product Category Selection page - GET REQUEST.*/
+router.get('/selectcategory',function(req,res, next ) {
+  res.render('productCategory');
+});
 
- /*Product Choice Clothing page - GET REQUEST*/
- router.get('/clothing',function(req,res, next ) {
-   res.render('productChoiceClothing');
-    });
+/*Product Choice Selection page - GET REQUEST.*/
+router.get('/selectproduct',function(req,res, next ) {
+  res.render('productChoice');
+});
 
- /*Product Choice Electronics page - GET REQUEST*/
- router.get('/electronics',function(req,res, next ) {
-   res.render('productChoiceElectronics');
-      });
- /*Product Choice Cosmetics page - GET REQUEST*/
- router.get('/cosmetics',function(req,res, next ) {
-   res.render('productChoiceCosmetics');
-        });
+/*Product Choice Clothing page - GET REQUEST.*/
+router.get('/clothing',function(req,res, next ) {
+  res.render('productChoiceClothing');
+});
 
- /*Product Choice Home page - GET REQUEST*/
- router.get('/home',function(req,res, next ) {
-   res.render('productChoiceHome');
-          });
+/*Product Choice Electronics page - GET REQUEST.*/
+router.get('/electronics',function(req,res, next ) {
+  res.render('productChoiceElectronics');
+});
 
- /*Final Feedback Survey page - GET REQUEST*/
- router.get('/surveyandfeedback',function(req,res, next ) {
-   res.render('surveyAndFeedback');
-   });
+/*Product Choice Cosmetics page - GET REQUEST.*/
+router.get('/cosmetics',function(req,res, next ) {
+  res.render('productChoiceCosmetics');
+});
 
- /*Thank you page - GET REQUEST*/
- router.get('/thankyou',function(req,res, next ) {
-    res.render('thankYouPage');
-    });
+/*Product Choice Home page - GET REQUEST.*/
+router.get('/home',function(req,res, next ) {
+  res.render('productChoiceHome');
+});
+
+/*Delivery Choice - GET REQUEST.*/
+router.get('/delivery',function(req,res, next ) {
+  res.render('productDeliveryChoice');
+});
+
+/*Final Feedback Survey page - GET REQUEST.*/
+router.get('/surveyandfeedback',function(req,res, next ) {
+  res.render('surveyAndFeedback');
+});
+
+/*Thank you page - GET REQUEST.*/
+router.get('/thankyou',function(req,res, next ) {
+  res.render('thankYouPage');
+});
 
 module.exports = router;
