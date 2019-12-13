bc9a679210003b899d0162f30ffd3662897c7179
Anastasiia Kurdydyk <37546139+akurdydy@users.noreply.github.com>
2019-12-13 16:49:25 +0100

    Add files via upload

---
+++ routes/index.js
@@ -0,0 +1,53 @@
+
+/*We are using the Router class.
+This enable us to export the routing logic as a module and use it in the main app.js.*/
+
+var express = require('express');
+
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next ) {
+  res.render('index', { title: 'Express' });
+});
+
+ /*Product Category Selection page - GET REQUEST*/
+ router.get('/selectcategory',function(req,res, next ) {
+    res.render('productCategory');
+ });
+
+ /*Product Choice Selection page - GET REQUEST*/
+ router.get('/selectproduct',function(req,res, next ) {
+   res.render('productChoice');
+  });
+
+ /*Product Choice Clothing page - GET REQUEST*/
+ router.get('/clothing',function(req,res, next ) {
+   res.render('productChoiceClothing');
+    });
+
+ /*Product Choice Electronics page - GET REQUEST*/
+ router.get('/electronics',function(req,res, next ) {
+   res.render('productChoiceElectronics');
+      });
+ /*Product Choice Cosmetics page - GET REQUEST*/
+ router.get('/cosmetics',function(req,res, next ) {
+   res.render('productChoiceCosmetics');
+        });
+
+ /*Product Choice Home page - GET REQUEST*/
+ router.get('/home',function(req,res, next ) {
+   res.render('productChoiceHome');
+          });
+
+ /*Final Feedback Survey page - GET REQUEST*/
+ router.get('/surveyandfeedback',function(req,res, next ) {
+   res.render('surveyAndFeedback');
+   });
+
+ /*Thank you page - GET REQUEST*/
+ router.get('/thankyou',function(req,res, next ) {
+    res.render('thankYouPage');
+    });
+
+module.exports = router;
