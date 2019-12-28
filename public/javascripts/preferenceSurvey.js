b8dc7ac423d68d7c7f6a833ba868ecae06132611
AniSoph <58134536+AniSoph@users.noreply.github.com>
2019-12-28 18:10:48 +0100

    Update preferenceSurvey.js
    Change Questionnaire content NEP scale

--- public/javascripts/preferenceSurvey.js
+++ public/javascripts/preferenceSurvey.js
@@ -1,48 +1,68 @@
 
 var surveyJSON = {
-
+ "completedHtml": "<br>\n</br>",
+ "loadingHtml": "http://localhost:8080/selectcategory",
  "pages": [
   {
    "name": "page1",
    "elements": [
     {
      "type": "matrix",
-     "name": "We .... Please indicate the degree to which you agree or disagree with the following statements using the scale below",
+     "name": "Finally we have a few questions about yourself. Please indicate the degree to which you agree or disagree with the following statements",
      "columns": [
       {
        "value": "1",
-       "text": "Strongly Disagree "
-      },
-      "2",
-      "3",
-      "4",
-      "5",
-      "6",
-      {
-       "value": "7",
        "text": "Strongly Agree"
+      },
+      {
+       "value": "2",
+       "text": "Mildly Agree "
+      },
+      {
+       "value": "3",
+       "text": "Unsure"
+      },
+      {
+       "value": "4",
+       "text": "Mildly Disagree "
+      },
+      {
+       "value": "5",
+       "text": "Strongly Disagree"
       }
      ],
      "rows": [
       {
-       "value": "3",
-       "text": "If I didn’t engage in proenvironmental behaviors, I would worry that others would think of me as a bad person"
-      },
-      {
-       "value": "1",
-       "text": "I am personally motivated by my concern for the environment to engage in proenvironmental behavior"
-      },
-      {
-       "value": "2",
-       "text": "I try to make decisions that are good for the environment because concern for the environment is central to my core values"
+       "value": "8",
+       "text": "The balance of nature is strong enough to cope with the impacts of modern industrial nations"
       },
       {
        "value": "4",
-       "text": "It is important to me that other people are aware of the efforts I make to help protect the environment"
+       "text": "Human ingenuity will insure that we do NOT make the earth unlivable"
       },
       {
        "value": "5",
-       "text": "I try to make proenvironmental decisions to avoid social disapproval"
+       "text": "Humans are severely abusing the environment"
+      },
+      {
+       "value": "2",
+       "text": "Humans have the right to modify the natural environment to suit their needs"
+      },
+      {
+       "value": "3",
+       "text": "When humans interfere with nature it often produces disastrous consequences"
+      },
+      {
+       "value": "6",
+       "text": "The earth has plenty of natural resources if we just learn how to develop them"
+      },
+      {
+       "value": "7",
+       "text": "Plants and animals have as much right as humans to exist"
+      },
+      {
+       "value": "1",
+       "text": "We are approaching the limit of the number of people the earth can support"
       }
      ],
      "rowsOrder": "random",
@@ -57,38 +77,57 @@ var surveyJSON = {
    "elements": [
     {
      "type": "matrix",
-     "name": "Please indicate the degree to which you agree or disagree with the following statements using the scale below",
+     "name": "Please indicate the degree to which you agree or disagree with the following statements ",
      "columns": [
       {
        "value": "1",
-       "text": "Strongly Disagree "
-      },
-      "2",
-      "3",
-      "4",
-      "5",
-      "6",
-      {
-       "value": "7",
        "text": "Strongly Agree"
+      },
+      {
+       "value": "2",
+       "text": "Mildly Agree "
+      },
+      {
+       "value": "3",
+       "text": "Unsure "
+      },
+      {
+       "value": "4",
+       "text": "Mildly Disagree"
+      },
+      {
+       "value": "5",
+       "text": "Strongly Disagree"
       }
      ],
      "rows": [
       {
        "value": "1",
-       "text": "I avoid behaviors that would hurt the environment due to my personally important beliefs "
+       "text": "Despite our special abilities humans are still subject to the laws of nature"
       },
       {
        "value": "2",
-       "text": "I try to behave in proenvironmental ways because it is personally important to me "
+       "text": "The so-called “ecological crisis” facing humankind has been greatly exaggerated"
       },
       {
        "value": "3",
-       "text": "If I engage in behaviors that may be harmful to the environment, I feel like others judge me negatively"
+       "text": "The earth is like a spaceship with very limited room and resources"
       },
       {
        "value": "4",
-       "text": "I am motivated to engage in behaviors that help protect the environment so that other people think I am a good person"
+       "text": "Humans were meant to rule over the rest of nature"
+      },
+      {
+       "value": "5",
+       "text": "The balance of nature is very delicate and easily upset"
+      },
+      {
+       "value": "6",
+       "text": "Humans will eventually learn enough about how nature works to be able to control it"
+      },
+      {
+       "value": "7",
+       "text": "If things continue on their present course, we will soon experience a major ecological catastrophe"
       }
      ],
      "isAllRowRequired": true
@@ -98,14 +137,12 @@ var surveyJSON = {
    "questionsOrder": "random"
   }
  ],
- "showPageTitles": false,
  "showCompletedPage": false,
  "questionsOrder": "random",
  "showQuestionNumbers": "off",
  "pageNextText": "Complete",
  "completeText": "Complete",
 }
-
 /* Send Ajax request to web server*/
 function sendDataToServer(survey) {
     window.location.href="http://localhost:8080/selectcategory";
